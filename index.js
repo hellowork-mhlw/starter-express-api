3ced3bcfc513fe174677af5df80c549d749866c7
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-10-29 10:27:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    console.log("Just got a request2!")
+    res.send('Yo2!')
 })
 app.listen(process.env.PORT || 3000)
