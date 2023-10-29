6c6398ce465caee2997a3245e9887222e736f668
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-10-29 01:34:27 +0000

    add

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require('express')
+
+const nodemailer = require("nodemailer");
+
+
+
+
+
 const app = express()
 app.all('/', (req, res) => {
+
+console.log(nodemailer)
+if (process.env.MAIL_HOST) {
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: 465,
+        secure: true,
+        auth: {
+          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      });
+        // send mail with defined transport object
+  transporter.sendMail({
+    from: '"Fred Foo 👻" <'+process.env.MAIL_USER+'>', // sender address
+    to: "osaka.niconico@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  }).then(console.log);
+
+}
+
+
     console.log("Just got a request2!")
     res.send('Yo2!')
 })
